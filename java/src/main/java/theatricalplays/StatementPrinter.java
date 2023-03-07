3be3893895a330b2b4696e3b223149a603162bca
happylynx <happylynx@localhost>
2023-03-07 03:12:53 +0100

    Variables renamed

--- java/src/main/java/theatricalplays/StatementPrinter.java
+++ java/src/main/java/theatricalplays/StatementPrinter.java
@@ -7,29 +7,29 @@ import java.util.Map;
 public class StatementPrinter {
 
     public String print(Invoice invoice, Map<String, Play> plays) {
-        int totalAmount = 0;
+        int totalPriceInCents = 0;
         int volumeCredits = 0;
         String result = String.format("Statement for %s\n", invoice.customer);
 
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance(Locale.US);
+        NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (Performance performance : invoice.performances) {
             Play play = plays.get(performance.playID);
-            int thisAmount = 0;
+            int priceInCents = 0;
 
             switch (play.type) {
                 case "tragedy":
-                    thisAmount = 40000;
+                    priceInCents = 40000;
                     if (performance.audience > 30) {
-                        thisAmount += 1000 * (performance.audience - 30);
+                        priceInCents += 1000 * (performance.audience - 30);
                     }
                     break;
                 case "comedy":
-                    thisAmount = 30000;
+                    priceInCents = 30000;
                     if (performance.audience > 20) {
-                        thisAmount += 10000 + 500 * (performance.audience - 20);
+                        priceInCents += 10000 + 500 * (performance.audience - 20);
                     }
-                    thisAmount += 300 * performance.audience;
+                    priceInCents += 300 * performance.audience;
                     break;
                 default:
                     throw new Error("unknown type: ${play.type}");
@@ -41,10 +41,10 @@ public class StatementPrinter {
             if ("comedy".equals(play.type)) volumeCredits += performance.audience / 5;
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, numberFormat.format(thisAmount / 100), performance.audience);
-            totalAmount += thisAmount;
+            result += String.format("  %s: %s (%s seats)\n", play.name, currencyFormatter.format(priceInCents / 100), performance.audience);
+            totalPriceInCents += priceInCents;
         }
-        result += String.format("Amount owed is %s\n", numberFormat.format(totalAmount / 100));
+        result += String.format("Amount owed is %s\n", currencyFormatter.format(totalPriceInCents / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
     }
